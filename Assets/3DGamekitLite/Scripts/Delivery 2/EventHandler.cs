b3cf8dc9ab6d793134df4f252e9c5a3022a51a3e
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-12-16 20:24:58 +0100

    eventhandler

---
+++ Assets/3DGamekitLite/Scripts/Delivery 2/EventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Gamekit3D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void AddNewDamageEvent(Damageable character)
+    {
+
+    }
+
+}
