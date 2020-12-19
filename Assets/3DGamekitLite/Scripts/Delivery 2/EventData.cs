d8a82a9719a3de2dadb0a7b8adc33abdd927296c
Èric Canela <knela96@gmail.com>
2020-12-19 17:56:49 +0100

    created heatmap

--- Assets/3DGamekitLite/Scripts/Delivery 2/EventData.cs
+++ Assets/3DGamekitLite/Scripts/Delivery 2/EventData.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+public enum EventFilter
+{
+    Position,
+    Death,
+    LifeLost,
+    Fall
+};
 public class EventData
 {
     uint EventID;
+    public EventFilter type;
+    DateTime Timestamp;
 
     // We can get .json data from here
     public string GetJSON()
@@ -15,26 +25,26 @@ public class EventData
 }
 public class PlayerPositionEvent : EventData     // Player current position | Also used for Heatmap --------- Should we create a timer for saving this position every 3 seconds ?
 {
-    Vector3 position;
+    public Vector3 position;
 }
-public class DeathEvent : EventData    // Player death position & enemy who killed you | Also used for Heatmap
+public class PlayerDeathEvent : EventData    // Player death position & enemy who killed you | Also used for Heatmap
 {
-    Vector3 position;
-    string enemy;
+    public Vector3 position;
+    public string enemy;
 }
 public class PlayerFallsEvent : EventData    // Player fall position & type of surface where player has fallen | Also used for Heatmap
 {
-    Vector3 position;
-    string type; //  ¿String? Maybe there's an enum of the types of surface ---- MUST CHECK 
+    public Vector3 position;
+    public string type; //  ¿String? Maybe there's an enum of the types of surface ---- MUST CHECK 
 }
 public class EnemyKillsEvent : EventData    // Enemy position where killed the player & enemy type name
 {
-    Vector3 enemy_position;
+    public Vector3 enemy_position;
     string enemy;
 }
 public class PlayerLifeLostEvent : EventData    // Player lost life position & enemy that damaged you | Also used for Heatmap
 {
-    Vector3 position;
+    public Vector3 position;
     string enemy;
 }
 public class SwitchesTimeEvent : EventData    // Time that player takes to press each switch
@@ -48,7 +58,7 @@ public class TimePuzzleEvent : EventData    // Time to complete the puzzle
 }
 public class ObjectsDestroyedEvent : EventData   // Objects destroyed & current destruction time
 {
-    Vector3 position;
+    public Vector3 position;
     float time;
 }
 public class FindKeyEvent : EventData   // Time to find the key
@@ -61,6 +71,6 @@ public class TimeToFinishEvent : EventData   // Time to finish the level
 }
 public class PlayerPathEvent : EventData    // Player position and rotation for debug path
 {
-    Vector3 position;
-    Vector3 orientation;
+    public Vector3 position;
+    public Vector3 orientation;
 }
---
+++ Assets/Heatmap.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Heatmap : MonoBehaviour
+{
+    public int GridSizeX = 200, GridSizeY = 200;
+    public float map_originX = -34, map_originY = -40;
+    public float cubeSize = 1;
+    int[,] gridArray;
+    public GameObject heatMapCube;
+    public float transparency = 0.5f;
+
+    //--------------------------
+
+    public List<GameObject> instancedCubes;
+    public List<EventData> EventsList = new List<EventData>();
+
+    public EventFilter current_filter;
+
+    public Gradient ColorGradient;
+
+    public int maxCounts = 50;
+
+    public void CountEvents()
+    {
+        
+        gridArray = new int[GridSizeX, GridSizeY];//Remove for later
+
+        for (int i = 0; i < EventsList.Count; i++)
+        {
+            EventData eventData = EventsList[i];
+            if (eventData.type == current_filter)
+            {
+                float x = 0, y = 0;
+                switch (eventData.type)
+                {
+                    case (EventFilter.Position):
+                        x = ((PlayerPositionEvent)eventData).position.x;
+                        y = ((PlayerPositionEvent)eventData).position.z;
+                        break;
+                    case (EventFilter.Death):
+                        x = ((PlayerDeathEvent)eventData).position.x;
+                        y = ((PlayerDeathEvent)eventData).position.z;
+                        break;
+                    case (EventFilter.LifeLost):
+                        x = ((PlayerLifeLostEvent)eventData).position.x;
+                        y = ((PlayerLifeLostEvent)eventData).position.z;
+                        break;
+                    case (EventFilter.Fall):
+                        x = ((PlayerFallsEvent)eventData).position.x;
+                        y = ((PlayerFallsEvent)eventData).position.z;
+                        break;
+                };
+
+                int newX = (int)(x / cubeSize - map_originX / cubeSize);
+                int newY = (int)(y / cubeSize - map_originY / cubeSize);
+
+
+                if (newX > 0 && newY > 0 && newX < GridSizeX && newY < GridSizeY)
+                {
+                    gridArray[newX, newY]++;
+                }
+            }
+        }
+        /* Change dynamically but affects all data
+        for (int x = 0; x < GridSizeX; x++)
+        {
+            for (int y = 0; y < GridSizeY; y++)
+            {
+                if(maxCounts < gridArray[x, y])
+                {
+                    maxCounts = gridArray[x, y];
+                }
+            }
+        }*/
+    }
+
+    void VisualizeGrid()
+    {
+        for (int x = 0; x < GridSizeX; x++)
+        {
+            for (int y = 0; y < GridSizeY; y++)
+            {
+                if(gridArray[x, y] > 0)
+                    SpawnCube(x, y, gridArray[x, y]);
+            }
+        }
+    }
+
+    void SpawnCube(int x, int y, int count)
+    {
+        x = (int)(x * cubeSize + map_originX + cubeSize / 2);
+        y = (int)(y * cubeSize + map_originY + cubeSize / 2);
+        Vector3 pos = new Vector3(x, GetHeight(x, y), y);
+        GameObject cube = Instantiate(heatMapCube, pos, Quaternion.identity);
+        instancedCubes.Add(cube);
+        float f = Mathf.Clamp01((float)count / maxCounts);
+        Color c = ColorGradient.Evaluate(f);
+        c.a = transparency;
+        cube.GetComponent<Renderer>().material.SetColor("_Color", c);
+    }
+
+    private int GetHeight(int x, int y)
+    {
+        Vector3 pos = new Vector3(x, 10, y);
+        RaycastHit hit;
+        int layerMask = 1 << 16;
+        if (Physics.Raycast(pos,Vector3.down, out hit, Mathf.Infinity, layerMask))
+        {
+            return (int)(hit.point.y + cubeSize);
+        }
+        return 0;
+    }
+
+    void Start()
+    {
+        GridSizeX = (int)(cubeSize * (float)GridSizeX);
+        GridSizeY = (int)(cubeSize * (float)GridSizeY);
+
+        gridArray = new int[GridSizeX, GridSizeY];
+
+        heatMapCube.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            CountEvents();
+            PlayerPositionEvent pos1 = new PlayerPositionEvent();
+            pos1.position = GameObject.Find("Ellen").transform.position;
+            EventsList.Add(pos1);
+
+            foreach (var obj in instancedCubes)
+            {
+                DestroyImmediate(obj);
+            }
+            instancedCubes.Clear();
+
+            VisualizeGrid();
+        }
+    }
+
+}
