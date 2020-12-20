c2c59cc7d485b5b3216c9f4ca7bd449ec11722cf
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-12-20 23:11:30 +0100

    script for controlling UI visualization

---
+++ Assets/ManageDropdowns.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageDropdowns : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject surface;
+    
+    public void Manage(int val)
+    {
+        if (val == 0)
+        {
+            surface.SetActive(false);
+            enemy.SetActive(false);
+        }
+        if (val == 1)
+        {
+            enemy.SetActive(true);
+            surface.SetActive(false);
+        }
+        if (val == 2)
+        {
+            enemy.SetActive(true);
+            surface.SetActive(false);
+        }
+    }
+}
