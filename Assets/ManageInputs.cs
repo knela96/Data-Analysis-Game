df38b0cb3915e7304a813f1bfce3227e89307ff4
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-12-21 00:09:32 +0100

    get input

---
+++ Assets/ManageInputs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageInputs : MonoBehaviour
+{
+    private string input;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReadInputValue(string i)
+    {
+        input = i;
+        Debug.Log(input);
+    }
+}
